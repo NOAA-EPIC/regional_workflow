5cf747597cc85979d0c0971767fd8ee8112181e1
Mark A Potts <mark.potts@noaa.gov>
2022-06-01 14:05:27 -0400

    Added script to make plots with modified plot_allvars

---
+++ ush/make_plots.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Prior to submitting the script the following environment variables
+# must be set using export or setenv
+# HOMErrfs=/path-to/ufs-srweather-app/regional_workflow
+# EXPTDIR=/path-to/expt_dirs/your_experiment
+
+cd ${HOMErrfs}/ush/Python
+set -x
+
+source /etc/profile.d/modules.sh
+
+############
+# Python environment for Cheyenne 
+############
+ncar_pylib /contrib/GST/ncar_pylib/python_graphics
+
+############
+# Path to shape files
+############
+#Cheyenne:
+SHAPE_FILES=/contrib/GST/NaturalEarth
+
+export GLOBAL_VAR_DEFNS_FP="${EXPTDIR}/var_defns.sh"
+source ${GLOBAL_VAR_DEFNS_FP}
+export CDATE=${DATE_FIRST_CYCL}${CYCL_HRS}
+export FCST_START=3
+export FCST_END=${FCST_LEN_HRS}
+export FCST_INC=3
+
+# Usage statement:      Make sure all the necessary modules can be imported.
+#                       Five command line arguments are needed:
+#                       1. Cycle date/time in YYYYMMDDHH format
+#                       2. Starting forecast hour in HHH format
+#                       3. Ending forecast hour in HHH format
+#                       4. Forecast hour increment
+#                       5. EXPT_DIR: Experiment directory
+#                          -Postprocessed data should be found in the directory:
+#                            EXPT_DIR/YYYYMMDDHH/postprd/
+#                       6. CARTOPY_DIR:  Base directory of cartopy shapefiles
+#                          -File structure should be:
+#                            CARTOPY_DIR/shapefiles/natural_earth/cultural/*.shp
+
+python plot_allvars.py ${CDATE} ${FCST_START} ${FCST_END} ${FCST_INC} ${EXPTDIR} ${SHAPE_FILES} ${POST_OUTPUT_DOMAIN_NAME}
